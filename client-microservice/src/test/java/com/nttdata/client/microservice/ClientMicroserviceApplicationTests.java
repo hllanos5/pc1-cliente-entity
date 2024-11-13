97871d34e92b6852bc0498c486a655b6c9a18bad
Hans Jonathan Llanos Chavez <hllanosc@emeal.nttdata.com>
2024-11-12 23:07:36 -0500

    proyecto base

---
+++ client-microservice/src/test/java/com/nttdata/client/microservice/ClientMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.client.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
